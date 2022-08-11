Fix derivative (0.06 accuracy improvement)

logistic_regression.py
@@ -14,7 +14,6 @@ def softmax(Z):
 class LogisticRegression:
     def __init__(
             self,
-            n_classes,
             learning_rate,
             n_epochs,
             mini_batch_size=32,
@@ -25,9 +24,9 @@ class LogisticRegression:
     ):
         self.W = None
         self.b = None
+        self.C = None
         self.rng = np.random.default_rng(0)
 
-        self.n_classes = n_classes
         self.learning_rate = learning_rate
         self.n_epochs = n_epochs
         self.mini_batch_size = mini_batch_size
@@ -39,10 +38,10 @@ class LogisticRegression:
 
     def fit(self, X, y):
         n, m = X.shape
-        self.W = np.zeros((self.n_classes, n))
-        self.b = np.zeros((self.n_classes, 1))
+        self.W = np.zeros((self.C, n))
+        self.b = np.zeros((self.C, 1))
         Y = make_one_hot(y)
-        assert Y.shape[0] == self.n_classes, "Not all classes are represented in the training set"
+        self.C = len(np.unique(Y))
 
         n_mini_batches = math.ceil(m / self.mini_batch_size)
         v_dW, v_db = 0, 0
@@ -61,9 +60,17 @@ class LogisticRegression:
                 Z = self.W @ X_mb + self.b
                 A = softmax(Z)
 
-                dZ = - Y_mb * (1 - A) / self.mini_batch_size
+                dA = - Y_mb / (A * self.mini_batch_size)
+                dA_dZ = np.zeros((self.C, self.C, self.mini_batch_size))
+
+                for k in range(self.mini_batch_size):
+                    for i in range(self.C):
+                        for j in range(self.C):
+                            dA_dZ[i, j, k] = A[i, k] * (1 - A[i, k]) if i == j else - A[i, k] * A[j, k]
+                dZ = np.einsum('ij,ikj->kj', dA, dA_dZ)
+
                 dW = dZ @ X_mb.T
-                db = np.sum(dZ, axis=-1, keepdims=True)
+                db = np.sum(dZ, axis=1, keepdims=True)
 
                 v_dW = self.beta1 * v_dW + (1 - self.beta1) * dW
                 v_db = self.beta1 * v_db + (1 - self.beta1) * db
@@ -74,7 +81,7 @@ class LogisticRegression:
                 self.W -= self.learning_rate * v_dW / (np.sqrt(s_dW) + EPS)
                 self.b -= self.learning_rate * v_db / (np.sqrt(s_db) + EPS)
 
-            if self.verbose and (epoch % 10 == 0 or epoch == self.n_epochs - 1):
+            if self.verbose:
                 print(
                     f'epoch {epoch:2d} - '
                     f'loss: {self.cross_entropy(X, Y):.2f}, '