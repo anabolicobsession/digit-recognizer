eded69ceca79095c52f8625be7340e190e32e29a
whiplashz <pilvorm@gmail.com>
2022-08-13 17:27:47 +0200

    Remove the clipping with epsilon

--- functions/loss_functions.py
+++ functions/loss_functions.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-from utils.constants import EPS
-
 
 def cross_entropy(Y, A):
-    return - np.sum(Y * np.log(np.maximum(A, EPS))) / A.shape[1]
+    return - np.sum(Y * np.log(A)) / A.shape[1]
 
 
 def d_cross_entropy(Y, A):
