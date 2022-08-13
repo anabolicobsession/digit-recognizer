2bb2a19cc6dfe2292c193ebd42ed615aa59d6d79
whiplashz <pilvorm@gmail.com>
2022-08-13 16:39:11 +0200

    Add computation of final derivative

--- functions/activation_functions.py
+++ functions/activation_functions.py
@@ -7,20 +7,22 @@ def softmax(Z):
     return A
 
 
-def d_softmax(A):
-    ds = - np.expand_dims(A, 1) * np.expand_dims(A, 0)
-    diagonal = A * (1 - A)
+def d_softmax(A, dA=None):
+    if dA is None:
+        dZ = - np.expand_dims(A, 1) * np.expand_dims(A, 0)
+        diagonal = A * (1 - A)
 
-    for i in range(A.shape[1]):
-        np.fill_diagonal(ds[:, :, i], diagonal[:, i])
+        for i in range(A.shape[1]):
+            np.fill_diagonal(dZ[:, :, i], diagonal[:, i])
+    else:
+        dZ = A * (dA - np.sum(dA * A, axis=0, keepdims=True))
 
-    return ds
+    return dZ
 
 
 def relu(Z):
     return np.maximum(Z, 0)
 
 
-def d_relu(A):
-    return (A > 0).astype(float)
-
+def d_relu(A, dA=None):
+    return (A > 0).astype(float) if dA is None else dA * (A > 0).astype(float)
